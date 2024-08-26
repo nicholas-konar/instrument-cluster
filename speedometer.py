834eabfd24060c28592875e6b0ded45b9907d6ca
Nicholas Konar <nkonar@protonmail.com>
2024-08-26 15:11:48 -0400

    Simplify speed fn

--- speedometer.py
+++ speedometer.py
@@ -7,6 +7,10 @@ import signal
 wheel_size = 26
 
 
+def miles(r): return (wheel_size * math.pi) * r / 12 / 5280  # miles
+def hour(t): return (time.time() - t) / 60 / 60  # hours
+def mph(r, t): return miles(r) / hour(t)
+
 def speed_listener(state, stop_event):
     def speedometer():
         while not stop_event.is_set():
@@ -20,16 +24,10 @@ def speed_listener(state, stop_event):
 
 
 def current_speed(r, p, s):
-    real = calculate_speed(r, p) if r > 0 else s
-    potential = calculate_speed(1, p)
+    real = mph(r, p) if r > 0 else s
+    potential = mph(1, p)
     speed = min(real, potential)
     return math.floor(speed) if potential > 1.8 else 0  # jump to 0 early for aesthetics
-
-
-def calculate_speed(r, p):
-    t = (time.time() - p) / 60 / 60  # hours
-    d = (wheel_size * math.pi) * r / 12 / 5280  # miles
-    return d / t if t > 0 else 0
 
 
 def input_listener(state, stop_event):
