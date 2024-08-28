bbb3b48ce0e64a15f081d3de1cfbad99d215ebb6
Nicholas Konar <nkonar@protonmail.com>
2024-08-28 18:54:57 -0400

    Calculate speed by dwell time

--- speedometer.py
+++ speedometer.py
@@ -1,5 +1,4 @@
 import math
-from numbers import Number
 import time
 import threading
 
@@ -9,30 +8,30 @@ odometer = 69
 
 def distance(r): return (wheel_size * math.pi) * r
 def miles(inch): return distance(inch) / 12 / 5280  # miles
-def hour(t): return (time.time() - t) / 60 / 60  # hours
+def hour(t): return t / 60 / 60  # hours
 def mph(r, t): return miles(r) / hour(t)
 
 
-def current_speed(r, t, s):
-    real = mph(r, t) if r > 0 else s
-    potential = mph(1, t)
+def current_speed(revs, last, dwell, speed):
+    real = mph(revs, dwell) if revs > 0 else speed
+    potential = mph(1, time.time() - last)
     n = min(real, potential)
     # jump to 0 early for aesthetics
     return math.floor(n) if potential > 1.5 else 0
 
 
 def update_state(state):
-    r, t, s, o = state
+    revs, time, speed, odo, dwell = state
     state[0] = 0
-    state[2] = current_speed(r, t, s)
-    state[3] += miles(r)
+    state[2] = current_speed(revs, time, dwell, speed)
+    state[3] += miles(revs)
     return state
 
 
 class Speedometer:
     def __init__(self):
         # todo: get wheel size & odo
-        self.state = [0, time.time(), 0, odometer]
+        self.state = [0, time.time(), 0, odometer, 0]
         self.thread = threading.Thread(target=self.run, daemon=True)
         self.stop_event = threading.Event()
         self.lock = threading.Lock()
@@ -44,10 +43,9 @@ class Speedometer:
 
     def run(self):
         while not self.stop_event.is_set():
-            with self.lock:
-                print(self.state)
-                self.state = update_state(self.state)
-                time.sleep(0.1)
+            print(self.state)
+            self.state = update_state(self.state)
+            time.sleep(0.1)
 
     def speed(self):
         return self.state[2]
@@ -56,6 +54,9 @@ class Speedometer:
         return self.state[3]
 
     def trigger_sensor(self):
-        t = time.time()
+        now = time.time()
+        duration = now - self.state[1]
+        print('dwell time', duration)
         self.state[0] += 1
-        self.state[1] = t
+        self.state[1] = now
+        self.state[4] = duration
