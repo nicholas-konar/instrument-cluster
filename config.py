1375434cc7820b8c2d57822bb8188012f068305c
Nicholas Konar <nkonar@protonmail.com>
2024-08-29 04:57:02 -0400

    Config setup

---
+++ config.py
@@ -0,0 +1,18 @@
+import yaml
+
+
+def load_config():
+    with open('config.yaml') as f:
+        return yaml.safe_load(f)
+
+
+config = load_config()
+
+
+def read_config():
+    return config or load_config()
+
+
+def write_config(config):
+    with open('config.yaml', 'w') as f:
+        yaml.dump(config, f)
